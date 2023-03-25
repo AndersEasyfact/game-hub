daabda7f8264dd9cff8897b7ad72eb36f0a079b5
Anders <anders@easyfact.no>
2023-03-25 14:25:26 +0100

    Fetch the genres

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -10,7 +10,6 @@ const GameGrid = () => {
 
   return (
     <>
-      IsLoading={isLoading ? "true" : "false"}
       {error && <Text>{error}</Text>}
       <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} spacing={10}>
         {isLoading &&
