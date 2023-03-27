08c575a2ffcfb09c9230a0ad38d86e6301cacac5
Anders <anders@easyfact.no>
2023-03-27 12:02:30 +0200

    Refactor: Extract a query object

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,5 +1,6 @@
 import { CanceledError } from "axios";
 import { useEffect, useState } from "react";
+import { GameQuery } from "../App";
 import apiClient from "../services/api-client";
 import useData from "./useData";
 import { Genre } from "./useGenres";
@@ -18,16 +19,16 @@ export interface Game {
   metacritic: number;
 }
 
-const useGames = (selectedGenre: Genre | null, selectedPlatform: Platform | null) =>
+const useGames = (gameQuery: GameQuery) =>
   useData<Game>(
     "/games",
     {
       params: {
-        genres: selectedGenre?.id,
-        platforms: selectedPlatform?.id,
+        genres: gameQuery.genre?.id,
+        platforms: gameQuery.platform?.id,
       },
     },
-    [selectedGenre?.id, selectedPlatform?.id]
+    [gameQuery]
   );
 
 export default useGames;
